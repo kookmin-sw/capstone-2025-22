ed694aec5530a8311cf64c62c43a4138c3abf334
Yeojin Goo <alina059900@gmail.com>
2025-01-25 21:34:12 +0900

    - main.dart - theme 정의: backgroundColor - login_screen.dart - theme 이용해서 color 지정 - sign_up_screen.dart - 구현 시작함.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:capstone_2025/screens/login_screen.dart';
+import 'package:capstone_2025/screens/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      // 페이지 공통 백그라운드 컬러 지정
+      theme: ThemeData(scaffoldBackgroundColor: Color(0xFFF2F1F3)),
+      home: SignUpScreen(),
     );
   }
 }
