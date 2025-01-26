df57e9819a3bf758f579c30cda4dd53155742a45
kym8821 <louie9798@naver.com>
2025-01-26 20:52:54 +0900

    [feat] init project

---
+++ src/main/java/com/capstone/letmedrum/LetmedrumApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.letmedrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetmedrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetmedrumApplication.class, args);
+	}
+
+}
