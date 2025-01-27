bd1e9ede1880a668096aaf409e5d07bbbe95506a
Yeojin Goo <alina059900@gmail.com>
2025-01-27 21:57:25 +0900

    Feat: 회원 가입 경고 메세지 출력 설정
    1. 특정 bool값에 따라 경고 메세지가 출력되도록 연결
    2. 현재 버튼을 누르면 해당 칸에 대한 bool값이 반대로 바뀌며 경고 메세지가 출력/삭제됨 - test상태
    3. '중복 확인' 버튼 사이즈 수정
    
    현재 회원가입이 TextField 위젯으로 구현되어 있으나, 유효성 검사 측면에서 유리한 Form, TextFormField로 변경 예정

--- lib/screens/introPages/sign_up_screen.dart
+++ lib/screens/introPages/sign_up_screen.dart
@@ -1,3 +1,10 @@
+// 입력창에 입력 안됨
+// 버튼 함수 제대로 연결 안됨
+// 버튼 함수 상세 정의 안함
+// 제출 버튼 연결 안함
+// 뒤로 가기 버튼 동작 안함
+// API 여기서 사용하는 것 같은데 알아보기
+
 import 'package:capstone_2025/screens/introPages/widgets/build_text_field.dart';
 import 'package:capstone_2025/screens/introPages/widgets/intro_page_header.dart';
 import 'package:flutter/material.dart';
@@ -9,15 +16,39 @@ class SignUpScreen extends StatefulWidget {
   State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-void goToMain() {
-  return;
-}
-
-void clickButton() {
-  return;
-}
-
 class _SignUpScreenState extends State<SignUpScreen> {
+  bool isCorrectEmail = true;
+  bool isAlreadyUseEmail = true;
+  bool isCorrectNum = true;
+  bool isAlreadyUseName = true;
+  bool isCorrectFormat = true;
+  bool isCorrectPw = true;
+
+  void submitBtn() {
+    return;
+  }
+
+  void idAuth() {
+    setState(() {
+      isAlreadyUseEmail = !isAlreadyUseEmail;
+    });
+    print(isAlreadyUseEmail);
+  }
+
+  void numAuth() {
+    setState(() {
+      isCorrectNum = !isCorrectNum;
+    });
+    print(isCorrectNum);
+  }
+
+  void nameAuth() {
+    setState(() {
+      isAlreadyUseName = !isAlreadyUseName;
+    });
+    print(isAlreadyUseName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,15 +71,26 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 name: '아이디',
                 hintTxt: '본인 인증을 위한 이메일 주소를 입력해주세요.',
                 needBtn: true,
-                buttonName: '전송',
+                btnName: '전송',
+                clickedFunc: idAuth,
               ),
-              Text(
-                '이메일 주소 형식이 잘못됐습니다.' '이미 가입된 이메일 주소입니다.',
-                style: TextStyle(
+              if (!isCorrectEmail)
+                Text(
+                  '이메일 주소 형식이 잘못됐습니다.',
+                  style: TextStyle(
                     fontSize: 12,
                     color: Colors.red,
-                    fontWeight: FontWeight.w600),
-              ), // 조건 추가하기
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              if (!isAlreadyUseEmail)
+                Text(
+                  '이미 가입된 이메일 주소입니다.',
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.red,
+                      fontWeight: FontWeight.w600),
+                ),
               SizedBox(
                 height: 15,
               ),
@@ -56,8 +98,17 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 name: '인증번호',
                 hintTxt: '인증번호 6자리를 입력해주세요.',
                 needBtn: true,
-                buttonName: '확인',
+                btnName: '확인',
+                clickedFunc: numAuth,
               ),
+              if (!isCorrectNum)
+                Text(
+                  '인증번호가 틀렸습니다.',
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.red,
+                      fontWeight: FontWeight.w600),
+                ),
               SizedBox(
                 height: 15,
               ),
@@ -65,8 +116,17 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 name: '닉네임',
                 hintTxt: '2 ~ 8자',
                 needBtn: true,
-                buttonName: '중복확인',
+                btnName: '중복확인',
+                clickedFunc: nameAuth,
               ),
+              if (!isAlreadyUseName)
+                Text(
+                  '이미 가입된 닉네임입니다.',
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.red,
+                      fontWeight: FontWeight.w600),
+                ),
               SizedBox(
                 height: 15,
               ),
@@ -74,6 +134,14 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   name: '비밀번호',
                   hintTxt: '8~20자의 영문, 숫자, 특수문자 조합',
                   needBtn: false),
+              if (!isCorrectFormat)
+                Text(
+                  '8~20자의 영문, 숫자, 특수문자를 모두 포함해주세요.',
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.red,
+                      fontWeight: FontWeight.w600),
+                ),
               SizedBox(
                 height: 15,
               ),
@@ -82,14 +150,24 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 hintTxt: '비밀번호를 한 번 더 입력해주세요.',
                 needBtn: false,
               ),
+              if (!isCorrectPw)
+                Text(
+                  '비밀번호가 일치하지 않습니다.',
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.red,
+                      fontWeight: FontWeight.w600),
+                ),
               SizedBox(
                 height: 35,
               ),
               SizedBox(
                 width: 200,
                 child: ButtonForm(
-                  buttonName: '제출',
+                  // 버튼 비활성화 추가하기
+                  btnName: '제출',
                   buttonColor: Color(0xFF424242),
+                  clickedFunc: submitBtn,
                 ),
               )
             ],
@@ -101,15 +179,18 @@ class _SignUpScreenState extends State<SignUpScreen> {
 }
 
 class InputForm extends StatelessWidget {
-  final String name, hintTxt, buttonName;
+  final String name, hintTxt, btnName;
   final bool needBtn;
+  final VoidCallback? clickedFunc;
 
-  InputForm({
-    required this.name,
-    required this.hintTxt,
-    required this.needBtn,
-    this.buttonName = "",
-  });
+  InputForm(
+      {required this.name,
+      required this.hintTxt,
+      required this.needBtn,
+      this.btnName = "",
+      this.clickedFunc}) {
+    print(clickedFunc);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -153,7 +234,7 @@ class InputForm extends StatelessWidget {
         if (needBtn)
           Expanded(
             flex: 2,
-            child: ButtonForm(buttonName: buttonName),
+            child: ButtonForm(btnName: btnName, clickedFunc: clickedFunc),
           ),
         Expanded(
           flex: needBtn ? 5 : 7,
@@ -165,15 +246,20 @@ class InputForm extends StatelessWidget {
 }
 
 class ButtonForm extends StatelessWidget {
-  const ButtonForm(
-      {super.key,
-      required this.buttonName,
-      this.buttonColor = const Color(0xFFD97D6C),
-      this.isTextBlack = false});
+  ButtonForm({
+    super.key,
+    required this.btnName,
+    this.buttonColor = const Color(0xFFD97D6C),
+    this.isTextBlack = false,
+    required this.clickedFunc,
+  }) {
+    print(clickedFunc);
+  }
 
-  final String buttonName;
+  final String btnName;
   final Color buttonColor;
   final bool isTextBlack;
+  final VoidCallback? clickedFunc;
 
   @override
   Widget build(BuildContext context) {
@@ -182,15 +268,14 @@ class ButtonForm extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12.0), // 버튼 테두리 둥글게
         ),
-        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // 버튼 크기 설정
+        padding:
+            EdgeInsets.symmetric(vertical: 16.5, horizontal: 10), // 버튼 크기 설정
         backgroundColor: buttonColor, // 버튼 배경색
       ),
-      onPressed: () {
-        // 로그인 버튼 클릭
-      },
+      onPressed: clickedFunc,
       child: Center(
         child: Text(
-          buttonName,
+          btnName,
           style: TextStyle(
             fontSize: 15.0,
             color: isTextBlack ? Colors.black : Colors.white,
