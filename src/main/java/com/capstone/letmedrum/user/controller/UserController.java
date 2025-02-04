a2c6420217fc9aa92fc1dfbe9ebb6273d8423710
kym8821 <louie9798@naver.com>
2025-02-04 15:08:34 +0900

    [feat] 사용자 정보 조회 등을 위한 UserController와 UserService 코드 작성

--- src/main/java/com/capstone/letmedrum/user/controller/UserController.java
+++ src/main/java/com/capstone/letmedrum/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.capstone.letmedrum.user.controller;
 
-import com.capstone.letmedrum.common.ApiResponse;
+import com.capstone.letmedrum.common.dto.ApiResponse;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,9 @@ public class UserController {
     @GetMapping("/test")
     public ApiResponse<String> testAuth(){
         return ApiResponse.success("success");
+    }
+    @GetMapping("/nickname-check/${nickname}")
+    public ApiResponse<String> checkNickname(@Param("nickname") String nickname){
+        return ApiResponse.success("nickname-check");
     }
 }
--- src/main/java/com/capstone/letmedrum/user/repository/UserRepository.java
+++ src/main/java/com/capstone/letmedrum/user/repository/UserRepository.java
@@ -13,4 +13,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u Where u.email=:email")
     Optional<User> findByEmail(@Param("email") String email);
+    @Query("SELECT u FROM User u Where u.nickname=:nickname")
+    Optional<User> findByNickname(@Param("nickname") String nickname);
 }
