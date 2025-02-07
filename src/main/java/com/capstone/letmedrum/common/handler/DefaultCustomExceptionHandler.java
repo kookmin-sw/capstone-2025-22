8ab26103044f75cd3a03305071a44e7fc201aad9
kym8821 <louie9798@naver.com>
2025-02-08 00:04:22 +0900

    [fix] 에러 로깅을 ExceptionHandler가 수행하도록 수정

--- src/main/java/com/capstone/letmedrum/common/handler/DefaultCustomExceptionHandler.java
+++ src/main/java/com/capstone/letmedrum/common/handler/DefaultCustomExceptionHandler.java
@@ -1,13 +1,16 @@
 package com.capstone.letmedrum.common.handler;
 
 import com.capstone.letmedrum.common.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class DefaultCustomExceptionHandler {
     @ExceptionHandler({RuntimeException.class})
     public ApiResponse<String> getDefaultExceptionApiResponse(Exception e){
+        log.error(e.getMessage(), e);
         return ApiResponse.error(e.getMessage());
     }
 }
---
+++ src/main/java/com/capstone/letmedrum/user/service/UserRetrieveService.java
@@ -0,0 +1,40 @@
+package com.capstone.letmedrum.user.service;
+
+import com.capstone.letmedrum.user.entity.User;
+import com.capstone.letmedrum.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    /**
+     * if user with nickname exists return user, else return null
+     * @param nickname - user's nickname, String
+     * @return User - User, nullable
+    * */
+    public User getUserOrNullByNickname(String nickname){
+        return userRepository.findByNickname(nickname).orElse(null);
+    }
+    /**
+     *  if user with email exists return user, else throw exception
+     * @param email - user's email, String
+     * @return User - User, not-null
+     * @throws RuntimeException if user not exists
+    * */
+    public User getUserOrExceptionByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("user not exists : " + email));
+    }
+    /**
+     *  if user with email exists return user, else return null
+     * @param email - user's email
+     * @return User - User, nullable
+     */
+    public User getUserOrNullByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElse(null);
+    }
+}
