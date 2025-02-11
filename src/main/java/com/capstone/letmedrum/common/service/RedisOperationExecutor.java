466efa1e01d0867c6a059addf9359ea429927243
kym8821 <louie9798@naver.com>
2025-02-11 17:04:56 +0900

    [refactor] Redis 단일 데이터 관리 클래스와 인터페이스 명칭 변경 - 단일 데이터 관리 인터페이스 명칭 변경 ( RedisService -> RedisSingleDataService ) - 단일 데이터 관리 클래스 명칭 변경 ( RedisSingleDataService -> RedisSingleDataServiceImpl)
    [fix] RedisOperationExecutor가 명령어 성공시 boolean 값을 리턴하도록 변경

--- src/main/java/com/capstone/letmedrum/common/service/RedisOperationExecutor.java
+++ src/main/java/com/capstone/letmedrum/common/service/RedisOperationExecutor.java
@@ -1,23 +1,25 @@
 package com.capstone.letmedrum.common.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class RedisOperationExecutor {
     /**
-     * run redis operation more safe
+     * run redis operation without exception
      * @param runnable redis operation
      * @return 1 if operation success
     * */
-    public int execute(Runnable runnable) {
+    public boolean execute(Runnable runnable) {
         try {
             runnable.run();
-            return 1;
+            return true;
         }catch (Exception e) {
             log.error(e.getMessage());
-            return 0;
+            return false;
         }
     }
 }
