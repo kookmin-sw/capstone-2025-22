8e948a045bc3a7f3a497453c29479fc68f92570a
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-02-16 19:49:32 +0900

    Feat: 앱을 가로 모드로 고정하는 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,18 @@ import 'package:capstone_2025/screens/introPages/login_screen.dart';
 import 'package:capstone_2025/screens/introPages/login_screen_google.dart';
 import 'package:capstone_2025/screens/introPages/sign_up_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 앱을 가로 모드로 고정
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
