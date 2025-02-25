28d632817467d1d23cceee5fc34b0cd621eada89
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-02-25 17:47:31 +0900

    Feat: 네비게이션바 포함 메인 화면 상위 파일 생성
    좌측은 네비게이션 바, 우측은 상황에 따른 화면이 뜨도록 구성해둔 파일
    이 파일에서 네비게이션 연결 및 전체 UI 관리

---
+++ lib/screens/mainPages/navigation_screens.dart
@@ -0,0 +1,60 @@
+import 'package:capstone_2025/screens/mainPages/my_page.dart';
+import 'package:capstone_2025/screens/mainPages/widgets/navigation_panel.dart';
+import 'package:flutter/material.dart';
+
+class NavigationScreens extends StatefulWidget {
+  @override
+  _NavigationScreensState createState() => _NavigationScreensState();
+}
+
+class _NavigationScreensState extends State<NavigationScreens> {
+  int _selectedIndex = 0; // 선택된 메뉴 인덱스
+
+  // 선택된 인덱스에 따라 오른쪽 화면을 변경하는 함수 - 추후 변경 필요
+  Widget _getPage(int index) {
+    switch (index) {
+      case 0:
+        return MyPage();
+      case 1:
+        return MyPage();
+      case 2:
+        return MyPage();
+      case 3:
+        return MyPage();
+      case 4:
+        return MyPage();
+      default:
+        return Center(child: Text("페이지를 선택해주세요"));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          // 좌측 네비게이션 바 (고정)
+          NavigationPanel(
+            selectedIndex: _selectedIndex,
+            onItemSelected: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+
+          // 오른쪽 화면 (선택된 메뉴에 따라 변경)
+          Expanded(
+            child: AnimatedSwitcher(
+              duration: Duration(milliseconds: 300), // 부드러운 화면 전환 애니메이션
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return FadeTransition(opacity: animation, child: child);
+              },
+              child: _getPage(_selectedIndex),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
