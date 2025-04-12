da5e75c0a239537e617dac705eceba44fff289d0
kym8821 <louie9798@naver.com>
2025-04-13 01:17:31 +0900

    [feat] 온셋 감지 모델 서버과 통신 기능 추가 및 연동 - 온셋 감지 모델 서버와 통신 기능 추가 및 로깅 기능 추가(AudioModelClient) - SocketConfig에 최대 메세지/버퍼 크기 설정 추가 - kafkaListener에서 온셋 감지 결과를 받아온 후 클라이언트에게 전달하는 기능 추가 ( CustomConsumer ) - 테스트용 html 파일에 온셋 감지 출력 기능 추가 ( index.html ) - 온셋 감지 서버에 보낼 요청 및 응답 DTO 추가 (OnsetDto)

--- audio-service/src/main/java/com/capstone/client/AudioModelClient.java
+++ audio-service/src/main/java/com/capstone/client/AudioModelClient.java
@@ -1,25 +1,59 @@
 package com.capstone.client;
 
-import com.capstone.dto.UserResponseDto;
-import com.capstone.response.CustomResponseDto;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
+import static com.capstone.dto.OnsetDto.*;
+
+@Slf4j
 @Component
 public class AudioModelClient {
-    private final WebClient userClient;
-    public AudioModelClient(@Qualifier(WebClientConfig.userClientName) final WebClient userClient) {
-        this.userClient = userClient;
+    private final WebClient audioClient;
+
+    public AudioModelClient(
+            @Value("${client.model-service-url}") String modelServiceUrl) {
+        this.audioClient = WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .filter((request, next) -> {
+                    log.info("[WebClient 요청] {} {}", request.method(), request.url());
+                    request.headers().forEach((name, values) ->
+                            values.forEach(value -> log.info("Header: {}={}", name, value)));
+                    return next.exchange(request);
+                })
+                .baseUrl(modelServiceUrl)
+                .build();
     }
-    public Mono<UserResponseDto> testUserExists(String email) {
-        return userClient.get()
-                .uri(builder -> builder.path("/users/email")
-                        .queryParam("email", email).build())
-                .exchangeToMono(response -> {
-                    if(response.statusCode().is4xxClientError()) return Mono.empty();
-                    return response.bodyToMono(String.class).map(resBody-> CustomResponseDto.resolveBody(resBody, UserResponseDto.class));
-                });
+
+    public Mono<OnsetResponseDto> getOnsetFromWav(OnsetRequestDto requestDto) {
+        return audioClient.post()
+                .uri("/onset")
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(requestDto)
+                .exchangeToMono(res -> {
+                    if(res.statusCode().isError()){
+                        return Mono.error(new RuntimeException("Error getting onset from WAV"));
+                    }
+                    return res.bodyToMono(OnsetResponseDto.class)
+                            .flatMap(onset -> {
+                                log.info("onset response {}", onset.toString());
+                                return Mono.just(onset);
+                            });
+                })
+                .log("onset webClient");
+    }
+
+    public String test(){
+        return audioClient.get()
+                .retrieve()
+                .bodyToMono(String.class).block();
     }
 }
