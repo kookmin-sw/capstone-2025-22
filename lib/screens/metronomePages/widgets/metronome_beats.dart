367bf0488434e9a2da448315b894b6851358392c
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-04-14 03:13:18 +0900

    Feat: 메트로놈 위젯으로 분할 후 개발
    1/4 박자일 때 빼고 구현 완료

--- lib/screens/metronomePages/widgets/metronome_beats.dart
+++ lib/screens/metronomePages/widgets/metronome_beats.dart
@@ -1,26 +1,13 @@
+import 'package:capstone_2025/providers/metronome_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MetronomeBeats extends StatefulWidget {
-  final int currentBeat;
-  final String timeSignature;
-  final bool currItemBase4;
-  final bool isSoundOn;
-  final bool isPlaying;
-
-  const MetronomeBeats({
-    required this.currentBeat,
-    required this.timeSignature,
-    required this.currItemBase4,
-    required this.isSoundOn,
-    required this.isPlaying,
-    super.key,
-  });
-
+class MetronomeBeats extends ConsumerStatefulWidget {
   @override
-  State<MetronomeBeats> createState() => _MetronomeBeatsState();
+  ConsumerState<MetronomeBeats> createState() => _MetronomeBeatsState();
 }
 
-class _MetronomeBeatsState extends State<MetronomeBeats>
+class _MetronomeBeatsState extends ConsumerState<MetronomeBeats>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _animation;
@@ -41,89 +28,126 @@ class _MetronomeBeatsState extends State<MetronomeBeats>
   }
 
   @override
-  void didUpdateWidget(MetronomeBeats oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.isPlaying &&
-        widget.currItemBase4 &&
-        widget.timeSignature == '1/4') {
-      _animationController.forward(from: 0.0);
-    }
-  }
-
-  @override
   void dispose() {
     _animationController.dispose();
     super.dispose();
   }
 
+  double bloomWidthandHeight(int idx) {
+    // bloom 효과의 크기 결정
+    bool currItemBase4 = ref.watch(currItemBase4Provider); // 4분음표 기준인지
+
+    if (!currItemBase4) {
+      // 4분음표 기준이 아닐 때
+      if (idx % 3 == 0) {
+        return 90; // 점4분음표 기준일 때
+      } else {
+        return 60; // 나머지 박자일 때
+      } // 점4분음표 기준일 때
+    }
+    return 90; // 기본값
+  }
+
+  List<dynamic> metronomeItemStyle(int idx) {
+    // 메트로놈 아이템 스타일 정의
+    String currBeatPattern = ref.watch(currBeatPatternProvider); // 현재 박자 패턴
+    bool isSelected = (idx == ref.watch(currItemProvider).toInt()); // 현재 박자인지
+    bool currItemBase4 = ref.watch(currItemBase4Provider); // 4분음표 기준인지
+
+    double width = 80;
+    double height = 80;
+    Color color = isSelected
+        ? const Color(0xffF28C79)
+        : const Color.fromARGB(255, 236, 122, 102);
+    Color borderColor = isSelected
+        ? Colors.transparent
+        : const Color.fromARGB(255, 125, 71, 61);
+
+    if (!currItemBase4) {
+      if (currBeatPattern == 'dot_quarter') {
+        if (idx % 3 != 0) {
+          width = 50;
+          height = 50;
+          color = isSelected
+              ? const Color(0xffF28C79)
+              : const Color.fromARGB(255, 108, 107, 106);
+          borderColor = isSelected ? Colors.transparent : Colors.black45;
+        }
+      } else if (currBeatPattern == "three") {
+        if (idx % 3 != 0) {
+          width = 50;
+          height = 50;
+        }
+      } else if (currBeatPattern == 'three_2') {
+        if (idx % 3 != 0) {
+          width = 50;
+          height = 50;
+          color = isSelected
+              ? const Color(0xffF28C79)
+              : (idx % 3 == 2)
+                  ? const Color.fromARGB(255, 236, 122, 102)
+                  : const Color.fromARGB(255, 108, 107, 106);
+          borderColor = isSelected
+              ? Colors.transparent
+              : (idx % 3 == 2)
+                  ? const Color.fromARGB(255, 125, 71, 61)
+                  : Colors.black45;
+        }
+      }
+    }
+    return [
+      width,
+      height,
+      color,
+      borderColor,
+    ];
+  }
+
   Widget metronomeItem(int idx) {
-    bool isSelected = (idx == widget.currentBeat);
-    bool shouldShowBloom = isSelected && widget.isPlaying;
+    // 메트로놈 아이템 생성 - 원 하나
+    final currItem = ref.watch(currItemProvider).toInt();
+    final isPlaying = ref.watch(isPlayingProvider);
+    final isSelected = (idx == currItem);
+    final shouldShowBloom = isSelected && isPlaying;
+    List<dynamic> style = metronomeItemStyle(idx);
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 50),
       child: Stack(
         clipBehavior: Clip.none,
         children: [
-          if (shouldShowBloom)
-            (widget.currItemBase4 && widget.timeSignature == '1/4')
-                ? AnimatedBuilder(
-                    animation: _animation,
-                    builder: (context, child) {
-                      return Positioned(
-                        left: -5,
-                        top: -5,
-                        child: Opacity(
-                          opacity: _animation.value,
-                          child: Container(
-                            width: 80,
-                            height: 80,
-                            decoration: BoxDecoration(
-                              color: const Color(0xffB95D4C),
-                              borderRadius: BorderRadius.circular(100),
-                              boxShadow: [
-                                BoxShadow(
-                                  color:
-                                      const Color(0xffD97D6C).withOpacity(0.7),
-                                  blurRadius: 5,
-                                  spreadRadius: 5,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  )
-                : Positioned(
-                    left: -5,
-                    top: -5,
-                    child: Container(
-                      width: (!widget.currItemBase4 && idx % 3 != 0) ? 60 : 80,
-                      height: (!widget.currItemBase4 && idx % 3 != 0) ? 60 : 80,
-                      decoration: BoxDecoration(
-                        color: const Color(0xffB95D4C),
-                        borderRadius: BorderRadius.circular(100),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color(0xffD97D6C).withOpacity(0.7),
-                            blurRadius: 5,
-                            spreadRadius: 5,
-                          ),
-                        ],
-                      ),
+          if (shouldShowBloom) // bloom 효과 대상 인지 - bloom효과 방식 상황에 따라 정의
+            Positioned(
+              // 4분음표 기준일 때도 고정된 bloom 효과
+              left: -5,
+              top: -5,
+              child: Container(
+                width: bloomWidthandHeight(idx),
+                height: bloomWidthandHeight(idx),
+                decoration: BoxDecoration(
+                  color: const Color(0xffB95D4C),
+                  borderRadius: BorderRadius.circular(100),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xffD97D6C).withOpacity(0.7),
+                      blurRadius: 5,
+                      spreadRadius: 5,
                     ),
-                  ),
+                  ],
+                ),
+              ),
+            ),
           Container(
-            width: (!widget.currItemBase4 && idx % 3 != 0) ? 50 : 70,
-            height: (!widget.currItemBase4 && idx % 3 != 0) ? 50 : 70,
+            // 메트로놈 원 기본 스타일 정의
+            width: style[0],
+            height: style[1],
             decoration: BoxDecoration(
-              color: isSelected
-                  ? const Color(0xffF28C79)
-                  : (!widget.currItemBase4 && idx % 3 != 0)
-                      ? const Color(0xff8C7571)
-                      : const Color.fromARGB(255, 123, 87, 81),
+              color: style[2],
               borderRadius: BorderRadius.circular(100),
+              border: Border.all(
+                color: style[3],
+                width: 4,
+              ),
             ),
           ),
         ],
@@ -132,9 +156,14 @@ class _MetronomeBeatsState extends State<MetronomeBeats>
   }
 
   List<Widget> generateMetronomeItems(int count) {
-    List<Widget> items = List.generate(count, (index) => metronomeItem(index));
+    // 메트로놈 아이템 생성
+    List<Widget> items = List.generate(
+        count, (index) => metronomeItem(index)); // List.generate 이용해서 자동 생성
 
-    if (widget.timeSignature != '9/8' && widget.timeSignature != '12/8') {
+    String timeSignature = ref.watch(selectedTimeSignatureProvider); // 현재 박자
+
+    if (timeSignature != '9/8' && timeSignature != '12/8') {
+      // 일렬로 출력
       return [
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -142,7 +171,8 @@ class _MetronomeBeatsState extends State<MetronomeBeats>
         )
       ];
     } else {
-      if (widget.timeSignature == '9/8') {
+      if (timeSignature == '9/8') {
+        // 2열로 출력 3 + 6
         return [
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -155,6 +185,7 @@ class _MetronomeBeatsState extends State<MetronomeBeats>
           ),
         ];
       } else {
+        // 12/8 박자일 때 - 2열로 출력 6 + 6
         return [
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -172,7 +203,11 @@ class _MetronomeBeatsState extends State<MetronomeBeats>
 
   @override
   Widget build(BuildContext context) {
-    int count = int.parse(widget.timeSignature.split('/')[0]);
+    // 최종 build 메서드
+    String timeSignature = ref.watch(selectedTimeSignatureProvider); // 현재 박자
+    int count =
+        int.parse(timeSignature.split('/')[0]); // 현재 박자에 따라 메트로놈 원 개수 결정
+
     return Container(
       decoration: const BoxDecoration(
         color: Color(0xff424242),
--- lib/screens/metronomePages/widgets/metronome_header.dart
+++ lib/screens/metronomePages/widgets/metronome_header.dart
@@ -1,46 +1,244 @@
+import 'package:capstone_2025/providers/metronome_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:capstone_2025/screens/mainPages/navigation_screens.dart';
+import 'dart:async';
 
-class MetronomeHeader extends StatefulWidget {
-  final VoidCallback onBackPressed;
-  final VoidCallback onPlayPressed;
-  final bool isPlaying;
-  final bool isHitVisible;
-  final bool isSoundOn;
-  final Function(bool) onHitVisibleChanged;
-  final Function(bool) onSoundOnChanged;
+class MetronomeHeader extends ConsumerStatefulWidget {
+  final AudioPlayer firstBeatPlayer;
+  final AudioPlayer otherBeatPlayer;
 
   const MetronomeHeader({
-    required this.onBackPressed,
-    required this.onPlayPressed,
-    required this.isPlaying,
-    required this.isHitVisible,
-    required this.isSoundOn,
-    required this.onHitVisibleChanged,
-    required this.onSoundOnChanged,
     super.key,
+    required this.firstBeatPlayer,
+    required this.otherBeatPlayer,
   });
 
   @override
-  State<MetronomeHeader> createState() => _MetronomeHeaderState();
+  ConsumerState<MetronomeHeader> createState() => _MetronomeHeaderState();
 }
 
-class _MetronomeHeaderState extends State<MetronomeHeader> {
-  final GlobalKey _popupMenuKey = GlobalKey();
+class _MetronomeHeaderState extends ConsumerState<MetronomeHeader> {
+  Timer? _timer; // 메트로놈 소리 출력을 위한 타이머
+  int next = 0;
+  int subNext = 0;
 
-  void _updatePopupMenu() {
-    (_popupMenuKey.currentState as dynamic).showButtonMenu();
+  @override
+  void initState() {
+    // 위젯이 생성될 때 호출
+    super.initState();
+    preloadSounds();
+  }
+
+  Future<void> preloadSounds() async {
+    // 사운드 미리 로드
+    await widget.firstBeatPlayer.setAudioSource(
+      AudioSource.asset('assets/sounds/metronome_first.wav'),
+    );
+    await widget.otherBeatPlayer.setAudioSource(
+      AudioSource.asset('assets/sounds/metronome_other.wav'),
+    );
+  }
+
+  void metronomeControl() {
+    final isPlaying = ref.read(isPlayingProvider);
+    if (isPlaying && _timer == null) return; // 상태 불일치 방지
+    if (!isPlaying && _timer != null) return;
+
+    if (_timer == null) {
+      // 재생 시작
+      try {
+        widget.firstBeatPlayer.stop();
+        widget.otherBeatPlayer.stop();
+      } catch (e) {
+        debugPrint('Audio stop error: $e');
+      }
+
+      ref.read(currItemProvider.notifier).state = 0;
+      if (ref.watch(isSoundOnProvider)) {
+        widget.firstBeatPlayer.seek(Duration.zero).then((_) {
+          widget.firstBeatPlayer.play();
+        });
+      }
+
+      _startMetronomeLoop();
+      ref.read(isPlayingProvider.notifier).state = true;
+    } else {
+      // 정지
+      stopMetronome();
+    }
+  }
+
+  void updateMetronomeTiming() {
+    if (_timer != null) {
+      _timer?.cancel();
+      _startMetronomeLoop(); // 인덱스 유지, 타이머만 새로 설정
+    }
+  }
+
+  void _startMetronomeLoop() {
+    int curBPM = ref.read(currBPMProvider).toInt();
+    final timeSignature = ref.read(selectedTimeSignatureProvider);
+    final currItemBase4 = ref.watch(currItemBase4Provider);
+    String currBeatPattern = ref.watch(currBeatPatternProvider);
+    int subBeatCount = 1;
+    if (timeSignature == '1/4') {
+      // 깜빡임 효과 위해 2개로 나누기
+      curBPM = curBPM * 2;
+      subBeatCount = 2;
+    }
+
+    if (currItemBase4) {
+      switch (currBeatPattern) {
+        case 'quarter':
+          curBPM = curBPM * 1;
+          break;
+        case 'two':
+          curBPM = curBPM * 2;
+          subBeatCount = subBeatCount * 2;
+          break;
+        case 'triplet':
+        case 'triplet2':
+          curBPM = curBPM * 3;
+          subBeatCount = subBeatCount * 3;
+          break;
+        case 'four':
+        case 'four_2':
+          curBPM = curBPM * 4;
+          subBeatCount = subBeatCount * 4;
+          break;
+      }
+    }
+
+    _timer = Timer.periodic(Duration(milliseconds: 60000 ~/ curBPM), (timer) {
+      final current = ref.read(currItemProvider);
+      final timeSignature = ref.read(selectedTimeSignatureProvider);
+      final beatCount = int.tryParse(timeSignature.split('/').first) ?? 4;
+      final currItemBase4 = ref.watch(currItemBase4Provider);
+
+      if (timeSignature == '1/4') {
+        if (subNext % subBeatCount == 0) {
+          next = (current == 0) ? -1 : 0;
+          ref.read(currItemProvider.notifier).state = next;
+        }
+      } else {
+        if (subNext % subBeatCount == 0) {
+          next = (current + 1) % beatCount;
+          ref.read(currItemProvider.notifier).state = next;
+        }
+      }
+
+      final effectiveIndex = subNext;
+
+      subNext = (subNext + 1) % (beatCount * subBeatCount);
+
+      if (ref.watch(isSoundOnProvider)) {
+        // 소리 재생을 위한 for loop
+        if (currItemBase4) {
+          if (effectiveIndex == 0) {
+            widget.firstBeatPlayer.seek(Duration.zero);
+            widget.firstBeatPlayer.play();
+          } else {
+            if (timeSignature == '1/4') {
+              // 4분음표 기준일 때
+              if ((currBeatPattern == 'quarter' ||
+                      currBeatPattern == 'two' ||
+                      currBeatPattern == 'triplet' ||
+                      currBeatPattern == 'four') &&
+                  effectiveIndex % subBeatCount == 0) {
+                widget.firstBeatPlayer.seek(Duration.zero);
+                widget.firstBeatPlayer.play();
+              } else {}
+            } else if (currBeatPattern == 'quarter' ||
+                currBeatPattern == 'two' ||
+                currBeatPattern == 'triplet' ||
+                currBeatPattern == 'four') {
+              widget.firstBeatPlayer.seek(Duration.zero);
+              widget.firstBeatPlayer.play();
+            } else if (currBeatPattern == 'triplet2') {
+              if (effectiveIndex % 3 != 1) {
+                widget.firstBeatPlayer.seek(Duration.zero);
+                widget.firstBeatPlayer.play();
+              } else {}
+            } else if (currBeatPattern == 'four_2') {
+              if (effectiveIndex % 4 == 0 || effectiveIndex % 4 == 3) {
+                widget.firstBeatPlayer.seek(Duration.zero);
+                widget.firstBeatPlayer.play();
+              } else {}
+            }
+          }
+        } else {
+          // 8분음표 기준일 때
+          if (next == 0) {
+            widget.firstBeatPlayer.seek(Duration.zero);
+            widget.firstBeatPlayer.play();
+          } else {
+            if (currBeatPattern == 'dot_quarter') {
+              if (next % 3 == 0) {
+                widget.otherBeatPlayer.seek(Duration.zero);
+                widget.otherBeatPlayer.play();
+              }
+            } else if (currBeatPattern == 'three') {
+              widget.otherBeatPlayer.seek(Duration.zero);
+              widget.otherBeatPlayer.play();
+            } else if (currBeatPattern == 'three_2') {
+              if (next % 3 == 0 || next % 3 == 2) {
+                widget.otherBeatPlayer.seek(Duration.zero);
+                widget.otherBeatPlayer.play();
+              } else {}
+            }
+          }
+        }
+      } else {
+        // 소리 재생 여부가 false일 때
+      }
+    });
+  }
+
+  void stopMetronome() {
+    _timer?.cancel();
+    _timer = null;
+    ref.read(isPlayingProvider.notifier).state = false;
+    ref.read(currItemProvider.notifier).state = -1;
+  }
+
+  void clickedBackBtn() {
+    // 뒤로가기 버튼 클릭 시 실행될 함수
+    if (_timer != null) {
+      // 타이머가 null이 아닐 때만 취소
+      stopMetronome();
+    }
+    ref.read(currBPMProvider.notifier).state = 120; // BPM 초기화
+    ref.read(currItemBase4Provider.notifier).state = true; // 4분음표 기준 초기화
+    ref.read(currBeatPatternProvider.notifier).state = 'quater'; // 박자 패턴 초기화
+    ref.read(selectedTimeSignatureProvider.notifier).state = '1/4'; // 박자 초기화
+    ref.read(isSoundOnProvider.notifier).state = true; // 소리 재생 여부 초기화
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+          builder: (context) => NavigationScreens(firstSelectedIndex: 4)),
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final isSoundOn = ref.watch(isSoundOnProvider);
     return Padding(
       padding: const EdgeInsets.only(top: 25),
       child: Row(
         children: [
           Expanded(
+            // 뒤로가기 버튼
             flex: 1,
             child: IconButton(
-              onPressed: widget.onBackPressed,
+              onPressed: clickedBackBtn,
               icon: const Icon(
                 Icons.arrow_back_ios,
                 size: 35,
@@ -49,8 +247,10 @@ class _MetronomeHeaderState extends State<MetronomeHeader> {
           ),
           const Spacer(flex: 4),
           Align(
+            // 메트로놈 재생 버튼
             alignment: Alignment.center,
             child: Container(
+              // 재생 버튼 배경
               height: 50,
               width: 150,
               decoration: BoxDecoration(
@@ -68,9 +268,11 @@ class _MetronomeHeaderState extends State<MetronomeHeader> {
               child: Transform.translate(
                 offset: const Offset(0, -12),
                 child: IconButton(
-                  onPressed: widget.onPlayPressed,
+                  onPressed: metronomeControl,
                   icon: Icon(
-                    widget.isPlaying ? Icons.stop : Icons.play_arrow,
+                    ref.watch(isPlayingProvider) // 재생 상태에 따라 아이콘 변경
+                        ? Icons.stop
+                        : Icons.play_arrow,
                     size: 60,
                   ),
                   highlightColor: Colors.transparent,
@@ -81,101 +283,15 @@ class _MetronomeHeaderState extends State<MetronomeHeader> {
           ),
           const Spacer(flex: 4),
           Expanded(
+            // 소리 설정 버튼
             flex: 1,
-            child: PopupMenuButton<int>(
-              icon: const Icon(
-                Icons.more_vert,
-                size: 35,
+            child: IconButton(
+              onPressed: () =>
+                  {ref.read(isSoundOnProvider.notifier).state = !isSoundOn},
+              icon: Icon(
+                isSoundOn ? Icons.volume_up : Icons.volume_off,
+                size: 45,
               ),
-              offset: const Offset(-40, 50),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  enabled: false,
-                  child: StatefulBuilder(
-                    builder: (context, setStateInternal) => Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 200,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text(
-                                "타격 표시",
-                                style: TextStyle(
-                                  fontSize: 17,
-                                  color: Colors.black38,
-                                ),
-                              ),
-                              const SizedBox(width: 20),
-                              Transform.scale(
-                                scaleX: 1.3,
-                                scaleY: 1.2,
-                                child: Switch(
-                                  value: widget.isHitVisible,
-                                  activeColor: const Color(0xffD97D6C),
-                                  inactiveThumbColor: Colors.grey,
-                                  inactiveTrackColor: Colors.grey.shade400,
-                                  onChanged: (val) {
-                                    widget.onHitVisibleChanged(val);
-                                    setStateInternal(() {});
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                PopupMenuItem(
-                  enabled: false,
-                  child: StatefulBuilder(
-                    builder: (context, setStateInternal) => Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 200,
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text(
-                                  "소리 재생",
-                                  style: TextStyle(
-                                    fontSize: 17,
-                                    color: Colors.black38,
-                                  ),
-                                ),
-                                const SizedBox(width: 20),
-                                Transform.scale(
-                                  scaleX: 1.3,
-                                  scaleY: 1.2,
-                                  child: Switch(
-                                    value: widget.isSoundOn,
-                                    activeColor: const Color(0xffD97D6C),
-                                    inactiveThumbColor: Colors.grey,
-                                    inactiveTrackColor: Colors.grey.shade400,
-                                    onChanged: (val) {
-                                      widget.onSoundOnChanged(val);
-                                      setStateInternal(() {});
-                                    },
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
             ),
           ),
         ],
