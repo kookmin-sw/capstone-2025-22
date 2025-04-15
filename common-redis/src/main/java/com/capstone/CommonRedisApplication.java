2e94ff3948bb6cbae5fe8519d74088ecaedf1b35
kym8821 <127489230+kym8821@users.noreply.github.com>
2025-04-16 02:52:20 +0900

    Audio service (#5)
    * [feat] 온셋 감지 모델 서버과 통신 기능 추가 및 연동
    - 온셋 감지 모델 서버와 통신 기능 추가 및 로깅 기능 추가(AudioModelClient)
    - SocketConfig에 최대 메세지/버퍼 크기 설정 추가
    - kafkaListener에서 온셋 감지 결과를 받아온 후 클라이언트에게 전달하는 기능 추가 ( CustomConsumer )
    - 테스트용 html 파일에 온셋 감지 출력 기능 추가 ( index.html )
    - 온셋 감지 서버에 보낼 요청 및 응답 DTO 추가 (OnsetDto)
    
    * [fix] socket 메세지 수신 관련 로깅 수정
    - base64가 너무 길기에 메세지 수신 여부만 로깅하도록 수정 ( AudioController )
    - AudioController에서 이미 로깅을 하기에 CustomProvider의 로깅 기능은 제거함
    
    * [fix] common 모듈 빌드 문제 수정
    - common 모듈 main 클래스 관련 문제 해결을 위해 bootjar.enabled를 disable함
    - common 모듈의 main 클래스 삭제
    
    * [test] AudioModelClientTest를 작성하기 위한 클래스만 생성해둠
    
    * [chore] 서브모듈 해시값 변경됨
    
    * [feat] 온셋 감지 결과 전달 경로 수정
    - 사용자 이메일 기반으로 온셋 결과를 전달하도록 수정
    - AudioMessageDto에 email 필드 추가
    - CustomConsumer가 결과를 전달하는 경로 수정
    - index.html에서 구독하는 경로 수정 및 전달하는 메세지 수정

--- common-redis/src/main/java/com/capstone/CommonRedisApplication.java
+++
@@ -1,8 +0,0 @@
-package com.capstone;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CommonRedisApplication {
-    public static void main(String[] args) {}
-}
