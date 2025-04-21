2c5af07b76ba66c0940e3a2eaf21fa5da54a3199
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-04-21 17:53:45 +0900

    Chore: import문 정리

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,4 @@
-import 'package:capstone_2025/screens/drumBasicsPages/drum_basics_page.dart';
 import 'package:capstone_2025/screens/drumSheetPages/drum_sheet_player.dart';
-import 'package:capstone_2025/screens/introPages/login_screen.dart';
-import 'package:capstone_2025/screens/introPages/login_screen_google.dart';
-import 'package:capstone_2025/screens/introPages/set_new_pw_screen.dart';
-import 'package:capstone_2025/screens/introPages/sign_up_screen.dart';
-import 'package:capstone_2025/screens/mainPages/edit_profile_screen.dart';
-import 'package:capstone_2025/screens/mainPages/musicsheet_detail.dart';
-import 'package:capstone_2025/screens/mainPages/my_page.dart';
 import 'package:capstone_2025/screens/mainPages/navigation_screens.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
