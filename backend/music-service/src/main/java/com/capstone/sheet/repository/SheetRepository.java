ea67881f94c6e1f01a4de6f8fc239bcbd816f3a6
kym8821 <louie9798@naver.com>
2025-05-02 03:52:58 +0900

    [fix] 불필요한 코드 제거 및 빌드 오류 수정 - settings.gradle의 루트 프로젝트 이름 수정 - SheetRepository에서 불필요한 import 제거

--- backend/music-service/src/main/java/com/capstone/sheet/repository/SheetRepository.java
+++ backend/music-service/src/main/java/com/capstone/sheet/repository/SheetRepository.java
@@ -6,12 +6,10 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface SheetRepository extends JpaRepository<Sheet, Integer> {
     @Modifying
-    @Transactional
     @Query("UPDATE Sheet s SET s.sheetInfo = :newSheetInfo WHERE s.sheetId = :sheetId")
     int updateSheetInfo(@Param("sheetId") int sheetId, @Param("newSheetInfo") byte[] newSheetInfo);
 }
