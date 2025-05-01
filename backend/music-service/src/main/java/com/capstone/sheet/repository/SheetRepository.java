d479416dba01141497456792711fe6dad1a23b01
kym8821 <louie9798@naver.com>
2025-05-01 16:04:27 +0900

    [feat] 비동기 기반 악보 및 사용자 악보 생성 기능 추가 - 시간이 오래 걸리는 악보 변환 기능은 비동기 처리 - 악보 관련 기능을 SheetUpdateService와 SheetCreateService로 분리 - SheetManageService에서 복합적인 악보 관련 기능을 처리하도록 변경

--- backend/music-service/src/main/java/com/capstone/sheet/repository/SheetRepository.java
+++ backend/music-service/src/main/java/com/capstone/sheet/repository/SheetRepository.java
@@ -2,8 +2,16 @@ package com.capstone.sheet.repository;
 
 import com.capstone.sheet.entity.Sheet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface SheetRepository extends JpaRepository<Sheet, Integer> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE Sheet s SET s.sheetInfo = :newSheetInfo WHERE s.sheetId = :sheetId")
+    int updateSheetInfo(@Param("sheetId") int sheetId, @Param("newSheetInfo") byte[] newSheetInfo);
 }
