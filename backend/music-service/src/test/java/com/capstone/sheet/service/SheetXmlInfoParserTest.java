c14b1bb6da30170c0577fd5aea7b460eafb39967
kym8821 <louie9798@naver.com>
2025-05-07 02:32:01 +0900

    [test] 악보 파싱 기능 테스트 코드 추가 및 필요한 파일 추가

---
+++ backend/music-service/src/test/java/com/capstone/sheet/service/SheetXmlInfoParserTest.java
@@ -0,0 +1,35 @@
+package com.capstone.sheet.service;
+
+import com.capstone.sheet.dto.musicXml.PartInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.nio.file.Files;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SheetXmlInfoParserTest {
+    @Autowired
+    private SheetXmlInfoParser sheetXmlInfoParser;
+
+    @Test
+    void parseXmlInfo() throws Exception{
+        // given
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource sheetXmlInfo = resourceLoader.getResource("classpath:sheet/sheet.xml");
+        byte[] sheetXmlBytes = Files.readAllBytes(sheetXmlInfo.getFile().toPath());
+        // when
+        List<PartInfo> partInfoList = sheetXmlInfoParser.parseXmlInfo(sheetXmlBytes);
+        // then
+        assert partInfoList.size() == 1;
+        assert partInfoList.get(0).getMeasureList().size() == 76;
+    }
+}
