63eb97835246254a3c0bdf12e301ddd15582102c
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-05-09 06:33:58 +0900

    chore: http 베포 서버 주소로 변경

--- frontend/lib/services/server_addr.dart
+++ frontend/lib/services/server_addr.dart
@@ -1,2 +1,2 @@
-String serverAddr = "10.0.2.2:28080"; // 서버 주소 (에뮬레이터용)
-// String serverAddr = "34.68.164.98:28080"; // 서버 주소
+// String serverAddr = "10.0.2.2:28080"; // 서버 주소 (에뮬레이터용)
+String serverAddr = "34.68.164.98:28080"; // 서버 주소
