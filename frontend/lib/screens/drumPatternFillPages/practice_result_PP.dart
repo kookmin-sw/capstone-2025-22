b774bdce38f164c3593b0cd946d83a9d79da50e4
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-05-14 11:05:58 +0900

    chore: 패턴 및 필인 페이지 이동 시 전해줄 인자 수정 (title -> index: String)

--- frontend/lib/screens/drumPatternFillPages/practice_result_PP.dart
+++ frontend/lib/screens/drumPatternFillPages/practice_result_PP.dart
@@ -301,7 +301,7 @@ class _PracticeResultPPState extends State<PracticeResultPP> {
                                                 MaterialPageRoute(
                                                   builder: (context) =>
                                                       PatternFillScreen(
-                                                    title: "${idx + 1}",
+                                                    index: "${idx + 1}",
                                                   ),
                                                 ),
                                               );
@@ -501,7 +501,7 @@ void openModal(
                       context,
                       MaterialPageRoute(
                         builder: (context) => PatternFillScreen(
-                          title: "$idx",
+                          index: "$idx",
                         ),
                       ),
                     );
