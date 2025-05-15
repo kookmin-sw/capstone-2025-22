6081622ae8939c5c5ae0f03373c0fe4418f50c8e
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-05-15 12:42:24 +0900

    chore: 디자인 변경 및 API 오류 수정(수정 중)

--- frontend/lib/screens/introPages/set_new_pw_screen.dart
+++ frontend/lib/screens/introPages/set_new_pw_screen.dart
@@ -1,14 +1,16 @@
 import 'package:capstone_2025/screens/introPages/find_pw_screen.dart';
 import 'package:capstone_2025/screens/introPages/login_screen.dart';
 import 'package:capstone_2025/screens/introPages/widgets/intro_page_header.dart';
+import 'package:capstone_2025/services/api_func.dart';
+import 'package:capstone_2025/services/storage_service.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 // 사용자가 본인 인증 완료 후 새로운 비밀번호를 설정하는 화면
 class SetNewPwScreen extends StatefulWidget {
-  const SetNewPwScreen({super.key});
+  const SetNewPwScreen({super.key, required this.emailToken});
+  final String emailToken; // 이메일 인증 토큰
 
   @override
   State<SetNewPwScreen> createState() => _SetNewPwScreenState();
@@ -21,33 +23,6 @@ class _SetNewPwScreenState extends State<SetNewPwScreen> {
   final _passwordController = TextEditingController(); // 새 비밀번호 입력 제어 컨트롤러
   final _confirmPasswordController =
       TextEditingController(); // 새 비밀번호 확인 제어 컨트롤러
-  final _storage = const FlutterSecureStorage();
-
-  // 기존 비밀번호와 동일한지 확인
-  Future<bool> _isSameAsOldPassword(String password) async {
-    final accessToken = await _storage.read(key: 'email_token');
-    final uri = Uri.parse('http://34.68.164.98:28080/verification/password');
-
-    final response = await http.post(
-      uri,
-      headers: {
-        'Content-Type': 'application/json',
-        'authorization': accessToken ?? '',
-      },
-      body: jsonEncode({'password': password}),
-    );
-
-    print(response.statusCode);
-
-    if (response.statusCode == 200) {
-      final result = jsonDecode(response.body);
-      print(result['body']);
-      return result['body'] == 'invalid'; // invalid이면 이전과 같음
-    } else {
-      print('비밀번호 검증 실패: ${response.statusCode}');
-      return false;
-    }
-  }
 
   // 비밀번호 유효성 검사
   String? _validatePassword(String? value) {
@@ -72,175 +47,146 @@ class _SetNewPwScreenState extends State<SetNewPwScreen> {
     return null;
   }
 
-  // 비밀번호 재설정 API 호출
-  Future<void> setNewPassword(
-      String newPassword, String emailToken, String accessToken) async {
-    final url = Uri.parse('https://34.68.164.98:28080/user/password'); // 요청 경로
-
-    try {
-      final response = await http.put(
-        url,
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $accessToken', // 사용자 access token 포함
-        },
-        body: jsonEncode({
-          'emailToken': emailToken, // 이메일 인증 완료 토큰
-          'password': newPassword, // 새로운 비밀번호
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        // 서버 응답이 정상(200)일 경우만
-        print('비밀번호 변경 성공: ${response.body}');
-      } else {
-        print('비밀번호 변경 실패: ${response.statusCode}');
-        throw Exception('비밀번호 변경 실패');
-      }
-    } catch (e) {
-      print('네트워크 오류: $e');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         body: Center(
-          child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              introPageHeader(
+                title: '비밀번호 재설정',
+                targetPage: FindPwScreen(), // 이게 의미 없음.
+                previous: true, // 바로 이전의 페이지로 이동
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 50,
-                    child: introPageHeader(
-                      title: '비밀번호 재설정',
-                      targetPage: FindPwScreen(), // 이게 의미 없음.
-                      previous: true, // 바로 이전의 페이지로 이동
-                    ),
-                  ),
-                  Text(
-                    "8~20자의 영문, 숫자, 특수문자를 포함한 비밀번호를 설정해주세요.",
-                    style: TextStyle(
-                      color: Color(0xff797979),
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  SizedBox(
-                    width: 400,
-                    child: Form(
-                      key: _formKey,
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            controller: _passwordController,
-                            obscureText: !_isPasswordVisible,
-                            validator: _validatePassword,
+              Text(
+                "8~20자의 영문, 숫자, 특수문자를 포함한 비밀번호를 설정해주세요.",
+                style: TextStyle(
+                  color: Color(0xff797979),
+                ),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: 400,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: !_isPasswordVisible,
+                        validator: _validatePassword,
 
-                            // build_text_field에 있던 속성 가져옴
-                            decoration: InputDecoration(
-                              hintText: '새 비밀번호',
-                              hintStyle: TextStyle(fontSize: 15),
-                              filled: true,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.circular(12.0), // 테두리를 둥글게 설정
+                        // build_text_field에 있던 속성 가져옴
+                        decoration: InputDecoration(
+                          hintText: '새 비밀번호',
+                          hintStyle: TextStyle(fontSize: 15),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.circular(12.0), // 테두리를 둥글게 설정
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(
+                                color: Colors.grey.shade400), // 기본 테두리 색
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(
+                                color: Color(0xFF424242),
+                                width: 2.0), // 포커스 시 테두리 색
+                          ),
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: IconButton(
+                              icon: Icon(
+                                _isPasswordVisible
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
                               ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12.0),
-                                borderSide: BorderSide(
-                                    color: Colors.grey.shade400), // 기본 테두리 색
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12.0),
-                                borderSide: BorderSide(
-                                    color: Color(0xFF424242),
-                                    width: 2.0), // 포커스 시 테두리 색
-                              ),
-                              suffixIcon: Padding(
-                                padding: const EdgeInsets.only(right: 8.0),
-                                child: IconButton(
-                                  icon: Icon(
-                                    _isPasswordVisible
-                                        ? Icons.visibility_off
-                                        : Icons.visibility,
-                                  ),
-                                  onPressed: () {
-                                    setState(
-                                      () {
-                                        _isPasswordVisible =
-                                            !_isPasswordVisible;
-                                      },
-                                    );
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    _isPasswordVisible = !_isPasswordVisible;
                                   },
-                                ),
-                              ),
+                                );
+                              },
                             ),
                           ),
-                          SizedBox(height: 22),
-                          TextFormField(
-                            controller: _confirmPasswordController,
-                            obscureText: !_isConfirmPasswordVisible,
-                            validator: _validateConfirmPassword,
-                            decoration: InputDecoration(
-                              hintText: '새 비밀번호 확인',
-                              hintStyle: TextStyle(fontSize: 15),
-                              filled: true,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.circular(12.0), // 테두리를 둥글게 설정
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12.0),
-                                borderSide: BorderSide(
-                                    color: Colors.grey.shade400), // 기본 테두리 색
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12.0),
-                                borderSide: BorderSide(
-                                    color: Color(0xFF424242),
-                                    width: 2.0), // 포커스 시 테두리 색
-                              ),
-                              suffixIcon: Padding(
-                                padding: const EdgeInsets.only(right: 8.0),
-                                child: IconButton(
-                                  icon: Icon(
-                                    _isConfirmPasswordVisible
-                                        ? Icons.visibility_off
-                                        : Icons.visibility,
-                                  ),
-                                  onPressed: () {
-                                    setState(
-                                      () {
-                                        _isConfirmPasswordVisible =
-                                            !_isConfirmPasswordVisible;
-                                      },
-                                    );
-                                  },
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                      SizedBox(height: 22),
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        obscureText: !_isConfirmPasswordVisible,
+                        validator: _validateConfirmPassword,
+                        decoration: InputDecoration(
+                          hintText: '새 비밀번호 확인',
+                          hintStyle: TextStyle(fontSize: 15),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.circular(12.0), // 테두리를 둥글게 설정
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(
+                                color: Colors.grey.shade400), // 기본 테두리 색
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(
+                                color: Color(0xFF424242),
+                                width: 2.0), // 포커스 시 테두리 색
+                          ),
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: IconButton(
+                              icon: Icon(
+                                _isConfirmPasswordVisible
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                              ),
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    _isConfirmPasswordVisible =
+                                        !_isConfirmPasswordVisible;
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 34),
-                  SizedBox(
-                      width: 400,
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            // 비밀번호 변경 완료하면 팝업창 띄움, 이후에 홈으로 이동 기능 추가해야 함
-                            final isDuplicate = await _isSameAsOldPassword(
-                                _passwordController.text); // 🔁 중복 검사
-                            if (isDuplicate) {
+                ),
+              ),
+              SizedBox(height: 34),
+              SizedBox(
+                  width: 400,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        // 비밀번호 변경 완료하면 팝업창 띄움
+                        try {
+                          print('emailToken: ${widget.emailToken}');
+                          final response = await putHTTP(
+                            '/users/password',
+                            {},
+                            {
+                              "newPassword": _passwordController.text,
+                              "emailToken": widget.emailToken,
+                            },
+                          );
+                          if (response['errMessage'] == null) {
+                            if (response['status'] == 200) {
+                              // 비밀번호 변경 성공
                               showDialog(
                                 context: context,
                                 builder: (BuildContext context) {
@@ -258,34 +204,43 @@ class _SetNewPwScreenState extends State<SetNewPwScreen> {
                                   );
                                 },
                               );
+                              // 로그인 화면으로 이동
+                              if (mounted) {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => LoginScreen()),
+                                );
+                              }
+                            } else {
+                              // 비밀번호 변경 실패
+                              print("비밀번호 재설정 실패: ${response['errMessage']}");
                             }
-                            // 로그인 화면으로 이동
-                            if (mounted) {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => LoginScreen()),
-                              );
-                            }
+                          } else {
+                            print("비밀번호 재설정 실패: ${response['errMessage']}");
                           }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                          padding: EdgeInsets.symmetric(vertical: 25.0),
-                          backgroundColor: Color(0xff424242),
-                        ),
-                        child: Text(
-                          '비밀번호 변경',
-                          style: TextStyle(
-                            fontSize: 15.0,
-                            color: Color(0xffffffff),
-                          ),
-                        ),
-                      ))
-                ],
-              )),
+                        } catch (e) {
+                          print('네트워크 오류: $e');
+                        }
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 25.0),
+                      backgroundColor: Color(0xff424242),
+                    ),
+                    child: Text(
+                      '비밀번호 변경',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Color(0xffffffff),
+                      ),
+                    ),
+                  ))
+            ],
+          ),
         ));
   }
 }
