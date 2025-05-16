99952ab2f5ede3191b1b81ed2f8f2053db48e9f1
kym8821 <louie9798@naver.com>
2025-05-16 22:33:28 +0900

    [feat] PatternMessageDto에 대한 producer, consumer 추가

--- backend/audio-service/src/main/java/com/capstone/config/KafkaConsumerConfig.java
+++ backend/audio-service/src/main/java/com/capstone/config/KafkaConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.capstone.config;
 
 import com.capstone.dto.AudioMessageDto;
+import com.capstone.dto.PatternMessageDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -20,29 +21,41 @@ import java.util.Map;
 public class KafkaConsumerConfig {
     private final KafkaConfig kafkaConfig;
     private final String groupId;
+
     public KafkaConsumerConfig(KafkaConfig kafkaConfig, @Value("${spring.kafka.consumer.group-id}") String groupId){
         this.groupId = groupId;
         this.kafkaConfig = kafkaConfig;
     }
-    public JsonDeserializer<AudioMessageDto> getAudioMessageDeserializer() {
-        JsonDeserializer<AudioMessageDto> deserializer = new JsonDeserializer<>(AudioMessageDto.class, new ObjectMapper());
+
+    public <T> JsonDeserializer<T> getJsonDeserializer(Class<T> clazz) {
+        JsonDeserializer<T> deserializer = new JsonDeserializer<>(clazz, new ObjectMapper());
         deserializer.addTrustedPackages("com.capstone.dto");
         deserializer.setUseTypeMapperForKey(true);
         return deserializer;
     }
-    @Bean
-    public ConsumerFactory<String, AudioMessageDto> consumerFactory(){
+
+    public <T> ConsumerFactory<String, T> consumerFactory(Class<T> clazz){
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.bootstrapServer);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), getAudioMessageDeserializer());
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), getJsonDeserializer(clazz));
     }
-    @Bean
-    public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory(){
-        ConcurrentKafkaListenerContainerFactory<String, AudioMessageDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+
+    public <T> ConcurrentKafkaListenerContainerFactory<String, T> createKafkaListenerFactory(Class<T> clazz){
+        ConcurrentKafkaListenerContainerFactory<String, T> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory(clazz));
         return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, AudioMessageDto> audioKafkaListenerFactory(){
+        return createKafkaListenerFactory(AudioMessageDto.class);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PatternMessageDto> patternKafkaListenerFactory(){
+        return createKafkaListenerFactory(PatternMessageDto.class);
     }
 }
