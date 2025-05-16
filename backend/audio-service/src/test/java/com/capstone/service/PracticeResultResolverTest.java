284b460e4a18a48b341d79f481d8888d02476165
kym8821 <louie9798@naver.com>
2025-05-17 03:31:25 +0900

    [test] 서로 다른 사용자 온셋 길이에 따른 1차 및 2차 채점 테스트 코드 추가

--- backend/audio-service/src/test/java/com/capstone/service/PracticeResultResolverTest.java
+++ backend/audio-service/src/test/java/com/capstone/service/PracticeResultResolverTest.java
@@ -59,7 +59,19 @@ class PracticeResultResolverTest {
                 .matchedUserOnsetIndices(new int[]{0, 1, 2, -1, 4})
                 .answerOnsetPlayed(new boolean[]{true, true, true, true, true})
                 .build();
-        onsetMatchResults = List.of(onsetMatchResultCorrect5, onsetMatchResultCorrect4);
+        OnsetMatchResult onsetMatchResultShort = OnsetMatchResult.builder()
+                .userOnset(List.of(1.0, 2.0, 3.0, 4.0))
+                .answerOnset(List.of(1.0, 2.0, 3.0, 4.5, 5.0))
+                .matchedUserOnsetIndices(new int[]{0, 1, 2, -1})
+                .answerOnsetPlayed(new boolean[]{true, true, true, false, false})
+                .build();
+        OnsetMatchResult onsetMatchResultLong = OnsetMatchResult.builder()
+                .userOnset(List.of(1.0, 2.0, 3.0, 5.0, 6.0, 7.0))
+                .answerOnset(List.of(1.0, 2.0, 3.0, 4.5, 5.0))
+                .matchedUserOnsetIndices(new int[]{0, 1, 2, 4, -1, -1})
+                .answerOnsetPlayed(new boolean[]{true, true, true, false, true})
+                .build();
+        onsetMatchResults = List.of(onsetMatchResultCorrect5, onsetMatchResultCorrect4, onsetMatchResultShort, onsetMatchResultLong);
         userOnset = List.of("0.0", "1.0", "2.0", "3.0", "4.0");
         drumPredictList = List.of(
                 new String[]{DrumInstrument.SNARE, DrumInstrument.TOM},
@@ -120,13 +132,42 @@ class PracticeResultResolverTest {
         // given
         OnsetMatchResult onsetMatchResultCorrect5 = onsetMatchResults.get(0);
         OnsetMatchResult onsetMatchResultCorrect4 = onsetMatchResults.get(1);
+        OnsetMatchResult onsetMatchResultShort = onsetMatchResults.get(2);
+        OnsetMatchResult onsetMatchResultLong = onsetMatchResults.get(3);
         List<String[]> drumPredictList = this.drumPredictList;
         MeasureInfo measureInfo = this.measureInfoMustBe100;
         // when
         List<Boolean> mustBeTTTTT = resultResolver.calculateFinalMatchingResult(onsetMatchResultCorrect5, drumPredictList, measureInfo);
         List<Boolean> mustBeTTTFT = resultResolver.calculateFinalMatchingResult(onsetMatchResultCorrect4, drumPredictList, measureInfo);
+        List<Boolean> mustBeTTTFFShort = resultResolver.calculateFinalMatchingResult(onsetMatchResultShort, drumPredictList, measureInfo);
+        List<Boolean> mustBeTTTFFLong = resultResolver.calculateFinalMatchingResult(onsetMatchResultLong, drumPredictList, measureInfo);
         // then
         assert mustBeTTTTT.equals(List.of(true, true, true, true, true));
         assert mustBeTTTFT.equals(List.of(true, true, true, false, true));
+        assert mustBeTTTFFShort.equals(List.of(true, true, true, false, false));
+        assert mustBeTTTFFShort.size() == onsetMatchResultShort.getAnswerOnset().size();
+        assert mustBeTTTFFLong.equals(List.of(true, true, true, false, false));
+        assert mustBeTTTFFLong.size() == onsetMatchResultLong.getAnswerOnset().size();
+    }
+
+    @Test
+    void calculateBeatMatchingResult_success(){
+        // given
+        OnsetMatchResult onsetMatchResultCorrect5 = onsetMatchResults.get(0);
+        OnsetMatchResult onsetMatchResultCorrect4 = onsetMatchResults.get(1);
+        OnsetMatchResult onsetMatchResultShort = onsetMatchResults.get(2);
+        OnsetMatchResult onsetMatchResultLong = onsetMatchResults.get(3);
+        // when
+        List<Boolean> mustBeTTTTT = resultResolver.calculateBeatMatchingResult(onsetMatchResultCorrect5);
+        List<Boolean> mustBeTTTFT = resultResolver.calculateBeatMatchingResult(onsetMatchResultCorrect4);
+        List<Boolean> mustBeTTTFFShort = resultResolver.calculateBeatMatchingResult(onsetMatchResultShort);
+        List<Boolean> mustBeTTTFTLong = resultResolver.calculateBeatMatchingResult(onsetMatchResultLong);
+        // then
+        assert mustBeTTTTT.equals(List.of(true, true, true, true, true));
+        assert mustBeTTTFT.equals(List.of(true, true, true, false, true));
+        assert mustBeTTTFFShort.equals(List.of(true, true, true, false, false));
+        assert mustBeTTTFFShort.size() == onsetMatchResultShort.getAnswerOnset().size();
+        assert mustBeTTTFTLong.equals(List.of(true, true, true, false, true));
+        assert mustBeTTTFTLong.size() == onsetMatchResultLong.getAnswerOnset().size();
     }
 }
