6faa447fe8ffa9f4461783bbd6ee0d1c79c2e45a
kym8821 <louie9798@naver.com>
2025-05-17 21:58:23 +0900

    [fix] 타입 불일치로 인해 pattern의 점수와 입력 점수를 비교하지 못하는 문제 수정

--- backend/music-service/src/main/java/com/capstone/sheet/repository/PatternRepository.java
+++ backend/music-service/src/main/java/com/capstone/sheet/repository/PatternRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface PatternRepository extends JpaRepository<Pattern, Long> {
-    @Query("select distinct p from Pattern p join PatternPractice pr on p.id=pr.pattern.id where pr.userEmail=:userEmail and pr.score>=:score")
+    @Query("select distinct p from Pattern p join PatternPractice pr on p.id=pr.pattern.id where pr.userEmail=:userEmail and CAST(pr.score AS int)>=:score")
     List<Pattern> findByUserEmailAndScoreGreaterThanEqual(String userEmail, int score);
 }
