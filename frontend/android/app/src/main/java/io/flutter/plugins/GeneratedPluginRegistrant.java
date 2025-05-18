5c6b57a3f02c311fd67a70de2c5fa7bc0eaf6b8f
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-05-18 12:54:47 +0900

    chore: 배포를 위한 파일, 패키지 추가 등

--- frontend/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ frontend/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -36,6 +36,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin file_selector_android, dev.flutter.packages.file_selector_android.FileSelectorAndroidPlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin firebase_core, io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new com.pichillilorenzo.flutter_inappwebview_android.InAppWebViewFlutterPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin flutter_inappwebview_android, com.pichillilorenzo.flutter_inappwebview_android.InAppWebViewFlutterPlugin", e);
